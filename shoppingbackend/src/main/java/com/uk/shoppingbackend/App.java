29c0ac1d65f36d5fec0ebbffa875c5cfc598da44
Umesh Kamble <umeshkamble036@gmail.com>
2019-12-10 23:35:16 +0530

    My Git Project SetUP Completed succesfully.

---
+++ shoppingbackend/src/main/java/com/uk/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.uk.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
